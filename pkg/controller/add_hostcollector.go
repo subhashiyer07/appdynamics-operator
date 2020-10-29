3041376e520a9e4c7f9f666620145aea7098cdf0
Vikyath Harekal <vikyath.harekal@appdynamics.com>
2020-10-29 12:44:35 +0530

    adding host-collector daemonset custom resource

---
+++ pkg/controller/add_hostcollector.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"appdynamics-operator/pkg/controller/hostcollector"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, hostcollector.Add)
+}
