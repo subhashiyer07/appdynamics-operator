c66f2a218e7363f13732127ed27d4a487d81d9c8
praksury <prakhar.suryawanshi@appdynamics.com>
2020-11-22 19:28:11 +0530

    CLUSTERMON-2155 Added basic support for infra-agent and cluster-collector to consume configs through file
    Operator will create a config file to be consumed by infra agent in the infra agent directory and
    another config file for cluster-collector in the collector directory from cluster-collector object specs.

--- pkg/apis/appdynamics/v1alpha1/clustercollector_types.go
+++ pkg/apis/appdynamics/v1alpha1/clustercollector_types.go
@@ -10,8 +10,31 @@ type ClustercollectorSpec struct {
 	Image              string                      `json:"image,omitempty"`
 	ServiceAccountName string                      `json:"serviceAccountName,omitempty"`
 	Resources          corev1.ResourceRequirements `json:"resources,omitempty"`
+	ControllerUrl      string                      `json:"controllerUrl"`
+	Account            string                      `json:"account,omitempty"`
+	ClusterName        string                      `json:"clusterName,omitempty"`
+	AccessSecret       string                      `json:"accessSecret,omitempty"`
+	NsToMonitorRegex   string                      `json:"nsToMonitorRegex,omitempty"`
+	NsToExcludeRegex   string                      `json:"nsToExcludeRegex,omitempty"`
+	LogLevel           string                      `json:"logLevel,omitempty"`
+	ClusterMonEnabled  bool                        `json:"clusterMonEnabled,omitempty"`
+	SystemConfigs      InfraAgentConfig            `json:"systemConfig,omitempty"`
 }
 
+
+type InfraAgentConfig struct {
+   CollectorLibSocketUrl    string `json:"collectorLibSocketUrl,omitempty"`
+   CollectorLibPort         string `json:"collectorLibPort,omitempty"`
+   HttpClientTimeOut            int `json:"httpClientTimeOut,omitempty"`
+   HttpBasicAuthEnabled     bool  `json:"httpBasicAuthEnabled,omitempty"`
+   ConfigChangeScanPeriod   int   `json:"configChangeScanPeriod,omitempty"`
+   ConfigStaleGracePeriod   int   `json:"configStaleGracePeriod,omitempty"`
+   DebugEnabled             bool  `json:"debugEnabled,omitempty"`
+   DebugPort                string `json:"debugPort,omitempty"`
+   LogLevel                 string `json:"logLevel,omitempty"`
+   ClientLibSendUrl         string  `json:"clientLibSendUrl,omitempty"`
+   ClientLibRecvUrl         string  `json:"clientLibRecvUrl,omitempty"`
+}
 // ClustercollectorStatus defines the observed state of Clustercollector
 type ClustercollectorStatus struct {
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
